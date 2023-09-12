359024b814959183d5716d2febd230b575a02e06
Gilmar Gomes <gomes.guit@gmail.com>
2023-09-12 19:54:17 -0300

    Sistema Caixa Eletrônico

---
+++ assets/script.js
@@ -0,0 +1,65 @@
+const saldoAtual = document.getElementById("saldoAtual");
+const extratoConta = document.getElementById("extratoConta");
+const valorInput = document.getElementById("valor");
+const btnSacar = document.querySelector("#btn-sacar");
+const btnDepositar = document.querySelector("#btn-depositar");
+
+function getCurrentTime() {
+  const data = new Date();
+  const hora = data.getHours();
+  const minuto = data.getMinutes();
+  const segundo = data.getSeconds();
+  const dia = data.toLocaleDateString();
+
+  return { hora, minuto, segundo, dia };
+}
+
+function saldoAtualDeposito() {
+  let valor = Number(prompt("Digite o valor total do seu dinheiro: "));
+  saldoAtual.innerHTML = valor.toFixed(2);
+}
+
+btnSacar.addEventListener("click", () => {
+  const valor = Number(valorInput.value);
+  const saldo = Number(saldoAtual.innerHTML);
+  const currentTime = getCurrentTime();
+
+  if (valorInput.value === "") {
+    alert("Digite um valor!!");
+    return;
+  }
+
+  if (saldo > 0 && saldo >= valor) {
+    saldoAtual.innerHTML = (saldo - valor).toFixed(2);
+    extratoConta.innerHTML += `<p class="saque" >Sacou R$${valor.toFixed(
+      2
+    )} às ${currentTime.hora}:${currentTime.minuto}:${
+      currentTime.segundo
+    } do dia ${currentTime.dia}</p>`;
+    valorInput.value = "";
+    valorInput.focus();
+  } else {
+    alert("Saldo insuficiente ou valor inválido.");
+  }
+});
+btnDepositar.addEventListener("click", () => {
+  const valor = Number(valorInput.value);
+  const saldo = Number(saldoAtual.innerHTML);
+  const currentTime = getCurrentTime();
+
+  if (valorInput.value === "") {
+    alert("Digite um valor!!");
+    return;
+  }
+
+  saldoAtual.innerHTML = (saldo + valor).toFixed(2);
+  extratoConta.innerHTML += `<p class="depos" >Depositou R$${valor.toFixed(
+    2
+  )} às ${currentTime.hora}:${currentTime.minuto}:${
+    currentTime.segundo
+  } do dia ${currentTime.dia}</p>`;
+  valorInput.value = "";
+  valorInput.focus();
+});
+
+saldoAtualDeposito();
